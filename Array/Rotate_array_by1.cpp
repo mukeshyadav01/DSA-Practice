68ef1950f7b53da18797badd5feeb791e41d002e
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-03 22:08:57 +0530

    Adding comment

--- Array/Rotate_array_by1.cpp
+++ Array/Rotate_array_by1.cpp
@@ -1,3 +1,6 @@
+// Program: Right Rotate Array by One
+// Description: Rotates array elements to the right by one position
+// Time Complexity: O(n)
 #include<iostream>
 using namespace std;
 int main()
@@ -6,6 +9,7 @@ int main()
     int temp,n;
     n= sizeof(arr)/sizeof(arr[0]);
     temp = arr[n-1];
+    // Shift every element one position to the right
     for(int i=n-2;i>=0;i--)
     {
         arr[i+1]=arr[i];
