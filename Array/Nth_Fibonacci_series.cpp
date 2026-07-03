02ce7202484410e3d9d3e00a1d57b95ce84a7289
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-03 08:40:16 +0530

    Add program to find nth Fibonacci number

---
+++ Array/Nth_Fibonacci_series.cpp
@@ -0,0 +1,20 @@
+// Program: Nth Fibonacci Number
+// Description: Finds the nth number in the Fibonacci sequence
+// Time Complexity: O(n)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    // take input from user
+    cout<<"Enter The number: ";
+    cin>>n;
+    int arr[1000];
+    // first =0;
+    // second =1;
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n-1;i++)
+    arr[i]=arr[i-1]+arr[i-2];
+    cout<<arr[n-1]<<" ";
+}
--- Array/README.md
+++ Array/README.md
@@ -4,3 +4,4 @@
 |------|-------------|---------|
 | Search_element.cpp | Finds index of element using linear search | Linear traversal |
 | Reverse_array.cpp  | Reverses array elements in place using two-pointer technique | Two-pointer technique |
+| Nth_fibonacci_number.cpp | Finds the nth number in the Fibonacci sequence | Dynamic Programming (Tabulation) |
