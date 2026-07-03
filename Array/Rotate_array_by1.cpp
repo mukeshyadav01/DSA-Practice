650bf7ef485b0aa3b8321bc8e6ea304d1c4d28e3
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-03 22:03:59 +0530

    Add program to right rotate array by one position

---
+++ Array/Rotate_array_by1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={3,5,5,8,9,4};
+    int temp,n;
+    n= sizeof(arr)/sizeof(arr[0]);
+    temp = arr[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        arr[i+1]=arr[i];
+    }
+    arr[0]=temp;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+}
