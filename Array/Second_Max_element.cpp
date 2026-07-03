a55e82370c319f3470e4e034c9f64369ae6c5124
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-03 10:34:48 +0530

    Add second largest element solution

---
+++ Array/Second_Max_element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int arr[]={4,8,9,11,15,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int ans=INT_MIN;
+    for(int i=0;i< n ; i++)
+    {
+        if(arr[i]>ans)
+        ans=arr[i];
+    }
+    int second= INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(ans != arr[i])
+        second = max(arr[i],second);
+    }
+    cout<<second;
+}
