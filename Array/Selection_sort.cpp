11230162b8ad3d16b275de60b0643adc4939b435
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-06 07:56:02 +0530

    Sorts an array in Ascending order using Selection sort

---
+++ Array/Selection_sort.cpp
@@ -0,0 +1,24 @@
+// Program : Selection Sort
+// Description : Sorts an array using selection sort
+// Time Complexity : O(n^2)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6]={4,8,9,11,7,4};
+    // Selection Sort
+    for(int i=0;i<5;i++)
+    {
+        int index =i;
+        for(int j=i+1;j<=5;j++)
+        {
+            if(arr[j]<arr[index])
+            index =j;
+        }
+        swap(arr[i],arr[index]);
+    }
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
